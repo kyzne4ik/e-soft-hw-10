bc130c90556a0031460b75fb5ee4a60c314d95de
Никита Тимофеев <freemorobskyr@gmail.com>
2026-06-08 20:02:13 +0500

    фикс бенчей: было 1000 стало 100 как по тз

--- src/demos/benchmark.js
+++ src/demos/benchmark.js
@@ -1,39 +1,39 @@
-import { prisma } from "../../prisma.js";
-import { db } from "../../db.js";
+import { prisma } from "../prisma.js";
+import { db } from "../db.js";
 
 const BENCH = { PRISMA: "[bench]-[ prisma ]", KNEX: "[bench]-[ knex ]" };
 
-async function run1000(func) {
-  for (let i = 0; i < 1000; i++) {
+async function run100(func) {
+  for (let i = 0; i < 100; i++) {
     await func();
   }
 }
 
 // ============== PRISMA =================
 console.time(BENCH.PRISMA);
-await run1000(prisma.post.findMany);
+await run100(() => prisma.post.findMany());
 console.timeEnd(BENCH.PRISMA);
 
 // ============== KNEX ===================
 console.time(BENCH.KNEX);
-await run1000(db("posts").select.bind(null, "*"));
+await run100(() => db("posts").select("*"));
 console.timeEnd(BENCH.KNEX);
 
 // ============== RESULT =================
 /*
 
-  NODE_ENV=production node /home/nesk/projects/e-soft/e-soft-hw-10/src/repositories/demos/benchmark.js
-  [bench]-[ prisma ]: 824.855ms
-  [bench]-[ knex ]: 258.966ms
+  NODE_ENV=production node /home/nesk/projects/e-soft/e-soft-hw-10/src/demos/benchmark.js
+  [bench]-[ prisma ]: 131.354ms
+  [bench]-[ knex ]: 44.637ms
 
 */
 /*
 
-  NODE_ENV=development node /home/nesk/projects/e-soft/e-soft-hw-10/src/repositories/demos/benchmark.js
-  [bench]-[ prisma ]: 956.823ms
-  [bench]-[ knex ]: 247.351ms
-
+  NODE_ENV=development node /home/nesk/projects/e-soft/e-soft-hw-10/src/demos/benchmark.js
+  [bench]-[ prisma ]: 144.703ms
+  [bench]-[ knex ]: 44.426ms
+  
 */
 
-// Как итог, скажу, что призма крутая, типобезопасная, 
-// но к сожалению медленная в сравнении с knex.
+// Как итог, скажу, что призма крутая, типобезопасная,
+// но медленнее в сравнении с knex.
